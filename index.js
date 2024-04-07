ff4b75a566771c495be26b1603062a8a573d6415
Adira Ferdy Agara <66525747+BorzXy@users.noreply.github.com>
2024-04-08 02:01:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const title = require("console-title");
 const ask = require("prompt-sync")();
 const gradient = require("gradient-string");
 const fetch = require('node-fetch'); //npm install node-fetch@^2.6.6
-var config = require("./config.json") //C:\Data\BorzXy\Full\GT\EcoScanner\players
+var config = require("./config.json");
 if (!fs.existsSync(config.playerdir)) return console.log("Error when Fetching to (" + config.playerdir + ")\n{Error Directory not found}");
 if (!fs.existsSync(config.worlddir)) return console.log("Error when Fetching to (" + config.worlddir + ")\n{Error Directory not found}");
 var playersdir = config.playerdir, worldsdir = config.worlddir;
+var playersdirtotal = fs.readdirSync(config.playerdir).length, worldsdirtotal = fs.readdirSync(config.worlddir).length;
 let date_ob = new Date();
 let date = ("0" + date_ob.getDate()).slice(-2);
 let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
@@ -27,6 +28,7 @@ if (month == 12) montas = "December";
 let year = date_ob.getFullYear();
 let hours = date_ob.getHours();
 var type = "", timeds = "";
+var version = "1.4\n"; //Dont change this to bypass version :)
 if (hours > 12) type = "PM";
 if (hours < 12) type = "AM";
 let minutes = date_ob.getMinutes();
@@ -35,12 +37,40 @@ var times = `${hours}:${minutes}:${seconds} ${type}`;
 var dates = `[${montas}-${date}-${year}]`;
 
 var log = `[${gradient('#f5d64c','#f5d64c')("•")}]`;
+var who = `[${gradient('#ff0011','#ff0011')("?")}]`
 var err = `[${gradient('#ff0011','#ff0011')("x")}]`;
+var warn = `[${gradient('#f5d64c','#f5d64c')("!")}]`;
+var results = `[${gradient('#ad5028','#ad5028')("~")}]`;
+
+
+/*function TypeIn (msg) {
+    for (let c of s + "\n") {
+        process.stdout.write(c);
+        process.stdout.flush();
+        setTimeout(() => {}, 50); // Delay of 50 milliseconds
+    }
+}*/
+
+const update_version = async () => {
+    var createnewfile = await fetch("https://raw.githubusercontent.com/BorzXy/EcoScanner/main/index.js").then(data => data.text());
+    console.log("Please wait, writing the file!")
+    var scriptName = path.basename(__filename);
+    fs.unlinkSync(scriptName);
+    setTimeout(() => {
+        fs.writeFileSync(scriptName, createnewfile);
+    }, 500);
+}
 
 const MasukMenu = async () => {
     console.clear();
+    var ckversion = await fetch("https://raw.githubusercontent.com/BorzXys/EcoScanner/main/ver").then(data => data.text());
+    if (version != ckversion) {
+        console.log("New Version foundednew version has been found, updating...");
+        update_version();
+        return;
+    }
     var ip = await fetch(`https://api.ipify.org`).then(data => data.text());
-    title("[MENU] | Date : " + dates)
+    title("[MENU] | Version : " + version + " | Date : " + dates)
     var banner = `${gradient('white','white')(`███████  ██████  █████  ███    ██ ███    ██ ███████ ██████  
 ██      ██      ██   ██ ████   ██ ████   ██ ██      ██   ██ 
 ███████ ██      ███████ ██ ██  ██ ██ ██  ██ █████   ██████  
@@ -48,17 +78,23 @@ const MasukMenu = async () => {
 ███████  ██████ ██   ██ ██   ████ ██   ████ ███████ ██   ██`)}
 
 ${log} Author : ${gradient('#f5d64c','#f5d64c')("Daritoc & BorzXy")}
-${log} Source : ${gradient('#f5d64c','#f5d64c')("Node.js (21.7.1) and Github")}
+${log} Github / Discord : ${gradient('#f5d64c','#f5d64c')("BorzXy / borzxy")}
+${log} Programming Language : ${gradient('#f5d64c','#f5d64c')("Node.js (21.7.1)")}
 ${log} IP : ${gradient('#f5d64c','#f5d64c')(ip)}
 ${log} Time : ${gradient('#f5d64c','#f5d64c')(times)}
 
-${log} Player Directory : ${gradient('#f5d64c', '#f5d64c')("(" + playersdir + ")")}
-${log} World Directory : ${gradient('#f5d64c', '#f5d64c')("(" + worldsdir + ")")}
+${log} Player Directory : ${gradient('#f5d64c', '#f5d64c')("(C:\\DATA\\Username\\BorzXy\\PT\\GT\\EcoScanner\\players\\)")}
+${log} World Directory : ${gradient('#f5d64c', '#f5d64c')("(C:\\DATA\\Username\\BorzXy\\PT\\GT\\EcoScanner\\worlds\\)")}
+${log} Registered Player Total : ${gradient('#f5d64c', '#f5d64c')(playersdirtotal)}
+${log} Registered World Total : ${gradient('#f5d64c', '#f5d64c')(worldsdirtotal)}
+
 
 ${log} Note : ${gradient('#f5d64c', '#f5d64c')("If you have any suggestion and Report a Problem, Feel free to Dm me at Discord (borzxy)")}
 
 ${log} [1] ${gradient('#f5d64c', '#f5d64c')("Role Scanner (Note: Only scan vip, mod, dev, superdev)")}
-${log} [2] ${gradient('#f5d64c', '#f5d64c')("Scan Economy (Note: Only Scan Dropped Item)")}
+${log} [2] ${gradient('#f5d64c', '#f5d64c')("Scan Economy (Note: Only Scan Dropped Item and Player Inventory)")}
+${log} [3] ${gradient('#f5d64c', '#f5d64c')("Info Player")}
+${log} [4] ${gradient('#f5d64c', '#f5d64c')("View Player Set (Note: You need items.json)")}
 ${log} [0] ${gradient('#f5d64c', '#f5d64c')("Exit")}
     `;
     console.clear();
@@ -85,6 +121,7 @@ function MenuTerakhir (type) {
 
 function Scanner (type) {
     if (type == 1) {
+        title("[Role Scanner] | Date : " + dates)
         fs.readdir(playersdir, (err, files) => {
             var filter = files.filter(f => f.split(".").pop() === "json");
             var vip = [], mod = [], dev = [], superdev = [];
@@ -110,7 +147,7 @@ function Scanner (type) {
                 }
             }
             console.clear();
-            console.log(`${log} [RESULT]:`)
+            console.log(`${results} [RESULT]:`)
             console.log(`${log} VIP : ${vip}`)
             console.log(`${log} Moderator : ${mod}`)
             console.log(`${log} Developer : ${dev}`)
@@ -121,8 +158,32 @@ function Scanner (type) {
             MenuTerakhir(menu);
         })
     } else if (type == 2) {
-        const itemid = ask("itemid? > ")
-		fs.readdir(worldsdir, (err, files) => {
+        title("[Scan Economy] | Date : " + dates)
+        const itemid = ask(who + " itemid? > ")
+        fs.readdir(playersdir, (err, files) => {
+            var filter = files.filter(f => f.split(".").pop() === "json");
+            var totalitem = [];
+            for (i = 0; i < filter.length; i++) {
+                try {
+                    const fileData = fs.readFileSync(path.join(playersdir, filter[i]));
+                    const json = JSON.parse(fileData.toString());
+                    let total = 0;
+                    for (_i = 0; _i < json.inv.length; _i++) {
+                        if (json.inv[_i].i == itemid) {
+                            total = json.inv[_i].c;
+                        }
+                    }
+                    totalitem.push(json.name + " | Total : " + total + "\n")
+                } catch (e) {
+                    console.log(e)
+                    return;
+                }
+            }
+            console.clear();
+            console.log(`${results} [RESULT]:`)
+            console.log(`${results} Players : ${totalitem}`)
+        })
+        fs.readdir(worldsdir, (err, files) => {
             var filter = files.filter(f => f.split(".").pop() === "json");
             var totalitem = [];
             for (i = 0; i < filter.length; i++) {
@@ -132,7 +193,9 @@ function Scanner (type) {
                     let total = 0;
                     for (_i = 0; _i < json.drop.length; _i++) {
                         if (json.drop[_i].i == itemid) {
-                            total = json.drop[_i].c;
+                            if (json.drop[_i].c != 0) {
+                                total = json.drop[_i].c;
+                            }
                         }
                     }
                     var owner = "";
@@ -141,20 +204,150 @@ function Scanner (type) {
                     } else {
                         owner = json.owner;
                     }
-                    totalitem.push(filter[i] + " | " + owner + " | Total : " + total + "\n")
+                    if (total != 0) {
+                        totalitem.push(filter[i] + " | " + owner + " | Total : " + total + "\n")
+                    }
                 } catch (e) {
                     console.log(e)
                     return;
                 }
             }
-            console.clear();
-            console.log(`${log} [RESULT]:`)
-            console.log(`${totalitem}`)
+            console.log(`${results} Worlds : ${totalitem}`)
             console.log(`\n${log} [1] Back to Main Menu`)
             console.log(`${log} [0] Exit`)
             var menu = ask(log + " > ");
             MenuTerakhir(menu);
         });
+    } else if (type == 3) {
+        title("[Info Player] | Date : " + dates)
+        var playername = ask(who + " Player GrowID > ");
+        if (!fs.existsSync(playersdir + playername + "_.json")) {
+            console.clear();
+            console.log(err + " Player not found!")
+            console.log(`\n${log} [1] Back to Main Menu`)
+            console.log(`${log} [0] Exit`)
+            var menu = ask(log + " > ");
+            MenuTerakhir(menu);
+            return;
+        }
+        const user = playersdir + playername + "_.json";
+        const data = require(user);
+        var ip = data.ip, rid = data.rid, mac = data.mac, wk = data.wk;
+        var level = data.level, xp = data.xp, growid = data.name, modname = data.modname, password = data.pass;
+        var growidc = "", growids = "";
+        var drtit = data._drt, legendtt = data.legend;
+        var drtits = "", legendtts = "";
+        var vip = data.vip, mod = data.mod, dev = data.dev, superdev = data.superdev;
+        var vips = "", mods = "", devs = "", superdevs = "";
+        if (drtit == 1 || drtit == true) drtits = "This user have Doctor Title";
+        if (legendtt == 1 || legendtt == true) legendtts = "This user have Legend Title";
+        if (vip == 1) {
+            growids = growid;
+            vips = "This user have VIP Role";
+        }
+        if (mod == 1) {
+            growids = "@" + growid;
+            mods = "This user have Moderator Role";
+        }
+        if (dev == 1) {
+            growids = "@" + growid;
+            devs = "This user have Developer Role";
+        }
+        if (superdev == 1) {
+            growids = "@" + growid;
+            superdevs = "This user have Super-Developer Role";
+        }
+        if (drtit == 1 || drtit == true) growids = "Dr. " + growids;
+        if (legendtt == 1 || legendtt == true) growids = growids + " of Legend";
+        console.clear();
+        console.log(`${results} [RESULT]:`)
+        console.log(`${log} [INFORMATION]:\n${results} Grow ID : ${growids}\n${warn} Password : ${password}\n${results} XP/LEVEL : ${xp} (${level})\n`); //\n${results} Mod Name : ${modnames}\n
+        console.log(`${log} [NETWORK]:\n${results} IP : ${ip}\n${results} RID : ${rid}\n${results} MAC : ${mac}\n${results} WK : ${wk}\n`)
+        console.log(`${log} [TITLE]:\n${results} ${drtits}\n${results} ${legendtts}\n`)
+        console.log(`${log} [ROLE]:\n${results} ${vips}\n${results} ${mods}\n${results} ${devs}\n${results} ${superdevs}`)
+        console.log(`\n${log} [1] Back to Main Menu`)
+        console.log(`${log} [0] Exit`)
+        var menu = ask(log + " > ");
+        MenuTerakhir(menu);
+    } else if (type == 4) {
+        title("[View Player Set] | Date : " + dates)
+        var playername = ask(who + " Player GrowID > ");
+        if (!fs.existsSync(playersdir + playername + "_.json")) {
+            console.clear();
+            console.log(err + " Player not found!")
+            console.log(`\n${log} [1] Back to Main Menu`)
+            console.log(`${log} [0] Exit`)
+            var menu = ask(log + " > ");
+            MenuTerakhir(menu);
+            return;
+        }
+        if (!fs.existsSync("./items.json")) {
+            console.clear();
+            console.log(err + " items.json not found\n" + err + " decode your items.dat at here https://gucktubeyt.github.io/GrowTools/")
+            console.log(`\n${log} [1] Back to Main Menu`)
+            console.log(`${log} [0] Exit`)
+            var menu = ask(log + " > ");
+            MenuTerakhir(menu);
+            return;
+        }
+        const user = playersdir + playername + "_.json";
+        const data = require(user);
+
+        var wing = data.back, hand = data.hand, hair = data.hair, pant = data.pants, feet = data.feet, face = data.face;
+        var mask = data.mask, ances = data.ances, necklace = data.necklace;
+        var wings = "", hands = "", hairs  = "", pants = "", feets = "", faces = "";
+        var masks = "", ancess  = "", necklaces  = "";
+        var wingc = "", handc = "", hairc  = "", pantc = "", feetc = "", facec = "";
+        var maskc = "", ancesc = "", necklacec = "";
+
+        const json = JSON.parse(fs.readFileSync("./items.json").toString());
+        for (const itemfinder of json.items) {
+            if (itemfinder.item_id == mask) {
+                maskc = itemfinder.name
+            }
+            if (itemfinder.item_id == ances) {
+                ancesc = itemfinder.name
+            }
+            if (itemfinder.item_id == necklace) {
+                necklacec = itemfinder.name
+            }
+            if (itemfinder.item_id == wing) {
+                wingc = itemfinder.name
+            }
+            if (itemfinder.item_id == hand) {
+                handc = itemfinder.name
+            }
+            if (itemfinder.item_id == hair) {
+                hairc = itemfinder.name
+            }
+            if (itemfinder.item_id == pant) {
+                pantc = itemfinder.name
+            }
+            if (itemfinder.item_id == feet) {
+                feetc = itemfinder.name
+            }
+            if (itemfinder.item_id == face) {
+                facec = itemfinder.name
+            }
+        }
+
+        if (maskc != "Blank") masks = results + " Mask : " + maskc + "\n";
+        if (ancesc != "Blank") ancess = results + " Ancestral : " + ancesc + "\n";
+        if (necklacec != "Blank") necklaces = results + " Necklace : " + necklacec + "\n";
+        if (wingc != "Blank") wings = results + " Wing : " + wingc + "\n";
+        if (handc != "Blank") hands = results + " Hand : " + handc + "\n";
+        if (hairc != "Blank") hairs = results + " Hair : " + hairc + "\n";
+        if (pantc != "Blank") pants = results + " Pant : " + pantc + "\n";
+        if (feetc != "Blank") feets = results + " Feet : " + feetc + "\n";
+        if (facec != "Blank") faces = results + " Face : " + facec + "\n";
+        
+        console.clear();
+        console.log(`${results} [RESULT]:`)
+        console.log(`${wings}${hands}${pants}${feets}${faces}${masks}${ancess}${necklaces}`)
+        console.log(`\n${log} [1] Back to Main Menu`)
+        console.log(`${log} [0] Exit`)
+        var menu = ask(log + " > ");
+        MenuTerakhir(menu);
     } else if (type == 0) {
         console.clear();
         console.log(log + " Bye Bye")
